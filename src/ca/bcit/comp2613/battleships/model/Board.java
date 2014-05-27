7217830cc26a9db7c94f399387367424786498cb
williamhu3@gmail.com <williamhu3@gmail.com>
2014-05-27 13:42:53 -0700

    test driver, model, util directories added

---
+++ src/ca/bcit/comp2613/battleships/model/Board.java
@@ -0,0 +1,5 @@
+package ca.bcit.comp2613.battleships.model;
+
+public class Board {
+
+}
---
+++ src/ca/bcit/comp2613/battleships/model/Ship.java
@@ -0,0 +1,14 @@
+package ca.bcit.comp2613.battleships.model;
+
+public class Ship extends Object {
+	
+	private Long id;
+	private Integer position;
+	private String color;
+	private Integer speed;
+	private Integer size;
+	private Integer hitPoints;
+	private Integer lives;
+	
+	public Ship() {}
+}
