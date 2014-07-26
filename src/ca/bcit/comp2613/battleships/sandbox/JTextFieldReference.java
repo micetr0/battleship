817afed955cff91656ff90df4ac6778d617e2e9c
Henry Chan <hchan@apache.org>
2014-07-25 17:38:02 -0700

    sample sandbox code

---
+++ src/ca/bcit/comp2613/battleships/sandbox/JTextFieldReference.java
@@ -0,0 +1,29 @@
+package ca.bcit.comp2613.battleships.sandbox;
+
+import java.util.HashMap;
+
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class JTextFieldReference {
+
+	private static HashMap<String, JTextField> textFieldMap = new HashMap<String, JTextField>();
+	
+	public static JTextField createTextField(String uniqueTextFieldName) {
+	    JTextField retval = new JTextField(uniqueTextFieldName);
+	    return retval;
+	}
+
+	public static void main(String[] args) {
+	    JPanel panel = new JPanel();
+	    panel.add(createTextField("coordiantes"));
+	    panel.add(createTextField("x"));
+	    panel.add(createTextField("y"));
+	    panel.add(createTextField("score"));
+	    
+	    
+	    JTextField x = textFieldMap.get("x");
+	    JTextField y = textFieldMap.get("y");
+	}
+	
+}
